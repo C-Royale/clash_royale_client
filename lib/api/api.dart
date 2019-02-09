6080a600880ed028b5f1d322495a26f961c3cef1
my9074 <313231068@qq.com>
2019-02-09 16:40:06 +0800

    Add api with player

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
-
 import 'package:dio/dio.dart';
+
 import '../utils/http.dart';
 
 // 获取 API 版本号
 Future<Response> getVersion(
     {dynamic data, Options options, CancelToken cancelToken}) async {
   Response response = await http.get('/version',
+      data: data, options: options, cancelToken: cancelToken);
+  return response;
+}
+
+// Player
+Future<Response> player(String id,
+    {dynamic data, Options options, CancelToken cancelToken}) async {
+  Response response = await http.get('/player/$id',
       data: data, options: options, cancelToken: cancelToken);
   return response;
 }
