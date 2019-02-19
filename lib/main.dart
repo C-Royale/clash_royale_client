c1f8934654e969f99e206587762516d048f67e6b
my9074 <313231068@qq.com>
2019-02-19 21:43:06 +0800

    Support dynamic switching themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'views/home/main.dart';
 import 'views/login/login.dart';
 
@@ -7,6 +8,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: new LoginPage());
+    return new DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => new ThemeData(
+              primarySwatch: Colors.indigo,
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return new MaterialApp(
+            title: 'Flutter Demo',
+            theme: theme,
+            home: new LoginPage(),
+          );
+        });
   }
 }
--- lib/views/setting/setting.dart
+++ lib/views/setting/setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 import '../login/login.dart';
 
 class Setting extends StatefulWidget {
@@ -13,16 +14,28 @@ class _SettingState extends State<Setting> {
         title: new Text('设置'),
       ),
       body: new Center(
-        child: RaisedButton(
-          child: Text("退出"),
-          onPressed: () {
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
-              (Route<dynamic> route) => false,
-            );
-          },
-        ),
+        child: ListView(children: <Widget>[
+          RaisedButton(
+            child: Text("退出"),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => LoginPage()),
+                (Route<dynamic> route) => false,
+              );
+            },
+          ),
+          RaisedButton(
+            child: Text("切换主题"),
+            onPressed: () {
+              DynamicTheme.of(context).setBrightness(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? Brightness.light
+                      : Brightness.dark);
+            },
+          )
+        ]),
       ),
     );
   }
