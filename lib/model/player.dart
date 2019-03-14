656e6fb9dee5d5ee9346ab21e7441a7044330d24
my9074 <313231068@qq.com>
2019-03-14 22:22:56 +0800

    Update player model

--- lib/model/player.dart
+++ lib/model/player.dart
@@ -16,7 +16,47 @@ class Player extends Object {
   @JsonKey(name: 'rank')
   int rank;
 
-  Player({this.tag, this.name, this.trophies, this.rank});
+  @JsonKey(name: 'arena')
+  Arena arena;
+
+  @JsonKey(name: 'clan')
+  Clan clan;
+
+  @JsonKey(name: 'stats')
+  Stats stats;
+
+  @JsonKey(name: 'games')
+  Games games;
+
+  @JsonKey(name: 'leagueStatistics')
+  LeagueStatistics leagueStatistics;
+
+  @JsonKey(name: 'deckLink')
+  String deckLink;
+
+  @JsonKey(name: 'currentDeck')
+  List<CurrentDeck> currentDeck;
+
+  @JsonKey(name: 'cards')
+  List<Cards> cards;
+
+  @JsonKey(name: 'achievements')
+  List<Achievements> achievements;
+
+  Player(
+      {this.tag,
+      this.name,
+      this.trophies,
+      this.rank,
+      this.arena,
+      this.clan,
+      this.stats,
+      this.games,
+      this.leagueStatistics,
+      this.deckLink,
+      this.currentDeck,
+      this.cards,
+      this.achievements});
 
   factory Player.fromJson(Map<String, dynamic> srcJson) =>
       _$PlayerFromJson(srcJson);
@@ -24,4 +64,499 @@ class Player extends Object {
   Map<String, dynamic> toJson() => _$PlayerToJson(this);
 
   Player.empty();
+}
+
+@JsonSerializable()
+class Arena extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'arena')
+  String arena;
+
+  @JsonKey(name: 'arenaID')
+  int arenaID;
+
+  @JsonKey(name: 'trophyLimit')
+  int trophyLimit;
+
+  Arena(
+    this.name,
+    this.arena,
+    this.arenaID,
+    this.trophyLimit,
+  );
+
+  factory Arena.fromJson(Map<String, dynamic> srcJson) =>
+      _$ArenaFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ArenaToJson(this);
+}
+
+@JsonSerializable()
+class Clan extends Object {
+  @JsonKey(name: 'tag')
+  String tag;
+
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'role')
+  String role;
+
+  @JsonKey(name: 'donations')
+  int donations;
+
+  @JsonKey(name: 'donationsReceived')
+  int donationsReceived;
+
+  @JsonKey(name: 'donationsDelta')
+  int donationsDelta;
+
+  @JsonKey(name: 'badge')
+  Badge badge;
+
+  Clan(
+    this.tag,
+    this.name,
+    this.role,
+    this.donations,
+    this.donationsReceived,
+    this.donationsDelta,
+    this.badge,
+  );
+
+  factory Clan.fromJson(Map<String, dynamic> srcJson) =>
+      _$ClanFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ClanToJson(this);
+}
+
+@JsonSerializable()
+class Badge extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'category')
+  String category;
+
+  @JsonKey(name: 'id')
+  int id;
+
+  @JsonKey(name: 'image')
+  String image;
+
+  Badge(
+    this.name,
+    this.category,
+    this.id,
+    this.image,
+  );
+
+  factory Badge.fromJson(Map<String, dynamic> srcJson) =>
+      _$BadgeFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$BadgeToJson(this);
+}
+
+@JsonSerializable()
+class Stats extends Object {
+  @JsonKey(name: 'clanCardsCollected')
+  int clanCardsCollected;
+
+  @JsonKey(name: 'tournamentCardsWon')
+  int tournamentCardsWon;
+
+  @JsonKey(name: 'maxTrophies')
+  int maxTrophies;
+
+  @JsonKey(name: 'threeCrownWins')
+  int threeCrownWins;
+
+  @JsonKey(name: 'cardsFound')
+  int cardsFound;
+
+  @JsonKey(name: 'favoriteCard')
+  FavoriteCard favoriteCard;
+
+  @JsonKey(name: 'totalDonations')
+  int totalDonations;
+
+  @JsonKey(name: 'challengeMaxWins')
+  int challengeMaxWins;
+
+  @JsonKey(name: 'challengeCardsWon')
+  int challengeCardsWon;
+
+  @JsonKey(name: 'level')
+  int level;
+
+  Stats(
+    this.clanCardsCollected,
+    this.tournamentCardsWon,
+    this.maxTrophies,
+    this.threeCrownWins,
+    this.cardsFound,
+    this.favoriteCard,
+    this.totalDonations,
+    this.challengeMaxWins,
+    this.challengeCardsWon,
+    this.level,
+  );
+
+  factory Stats.fromJson(Map<String, dynamic> srcJson) =>
+      _$StatsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$StatsToJson(this);
+}
+
+@JsonSerializable()
+class FavoriteCard extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'id')
+  int id;
+
+  @JsonKey(name: 'maxLevel')
+  int maxLevel;
+
+  @JsonKey(name: 'icon')
+  String icon;
+
+  @JsonKey(name: 'key')
+  String key;
+
+  @JsonKey(name: 'elixir')
+  int elixir;
+
+  @JsonKey(name: 'type')
+  String type;
+
+  @JsonKey(name: 'rarity')
+  String rarity;
+
+  @JsonKey(name: 'arena')
+  int arena;
+
+  @JsonKey(name: 'description')
+  String description;
+
+  FavoriteCard(
+    this.name,
+    this.id,
+    this.maxLevel,
+    this.icon,
+    this.key,
+    this.elixir,
+    this.type,
+    this.rarity,
+    this.arena,
+    this.description,
+  );
+
+  factory FavoriteCard.fromJson(Map<String, dynamic> srcJson) =>
+      _$FavoriteCardFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$FavoriteCardToJson(this);
+}
+
+@JsonSerializable()
+class Games extends Object {
+  @JsonKey(name: 'total')
+  int total;
+
+  @JsonKey(name: 'tournamentGames')
+  int tournamentGames;
+
+  @JsonKey(name: 'wins')
+  int wins;
+
+  @JsonKey(name: 'warDayWins')
+  int warDayWins;
+
+  @JsonKey(name: 'winsPercent')
+  double winsPercent;
+
+  @JsonKey(name: 'losses')
+  int losses;
+
+  @JsonKey(name: 'lossesPercent')
+  double lossesPercent;
+
+  @JsonKey(name: 'draws')
+  int draws;
+
+  @JsonKey(name: 'drawsPercent')
+  double drawsPercent;
+
+  Games(
+    this.total,
+    this.tournamentGames,
+    this.wins,
+    this.warDayWins,
+    this.winsPercent,
+    this.losses,
+    this.lossesPercent,
+    this.draws,
+    this.drawsPercent,
+  );
+
+  factory Games.fromJson(Map<String, dynamic> srcJson) =>
+      _$GamesFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$GamesToJson(this);
+}
+
+@JsonSerializable()
+class LeagueStatistics extends Object {
+  @JsonKey(name: 'currentSeason')
+  CurrentSeason currentSeason;
+
+  @JsonKey(name: 'previousSeason')
+  PreviousSeason previousSeason;
+
+  @JsonKey(name: 'bestSeason')
+  BestSeason bestSeason;
+
+  LeagueStatistics(
+    this.currentSeason,
+    this.previousSeason,
+    this.bestSeason,
+  );
+
+  factory LeagueStatistics.fromJson(Map<String, dynamic> srcJson) =>
+      _$LeagueStatisticsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$LeagueStatisticsToJson(this);
+}
+
+@JsonSerializable()
+class CurrentSeason extends Object {
+  @JsonKey(name: 'rank')
+  int rank;
+
+  @JsonKey(name: 'trophies')
+  int trophies;
+
+  @JsonKey(name: 'bestTrophies')
+  int bestTrophies;
+
+  CurrentSeason(
+    this.rank,
+    this.trophies,
+    this.bestTrophies,
+  );
+
+  factory CurrentSeason.fromJson(Map<String, dynamic> srcJson) =>
+      _$CurrentSeasonFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$CurrentSeasonToJson(this);
+}
+
+@JsonSerializable()
+class PreviousSeason extends Object {
+  @JsonKey(name: 'id')
+  String id;
+
+  @JsonKey(name: 'rank')
+  int rank;
+
+  @JsonKey(name: 'trophies')
+  int trophies;
+
+  @JsonKey(name: 'bestTrophies')
+  int bestTrophies;
+
+  PreviousSeason(
+    this.id,
+    this.rank,
+    this.trophies,
+    this.bestTrophies,
+  );
+
+  factory PreviousSeason.fromJson(Map<String, dynamic> srcJson) =>
+      _$PreviousSeasonFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$PreviousSeasonToJson(this);
+}
+
+@JsonSerializable()
+class BestSeason extends Object {
+  @JsonKey(name: 'id')
+  String id;
+
+  @JsonKey(name: 'rank')
+  int rank;
+
+  @JsonKey(name: 'trophies')
+  int trophies;
+
+  BestSeason(
+    this.id,
+    this.rank,
+    this.trophies,
+  );
+
+  factory BestSeason.fromJson(Map<String, dynamic> srcJson) =>
+      _$BestSeasonFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$BestSeasonToJson(this);
+}
+
+@JsonSerializable()
+class CurrentDeck extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'level')
+  int level;
+
+  @JsonKey(name: 'maxLevel')
+  int maxLevel;
+
+  @JsonKey(name: 'count')
+  int count;
+
+  @JsonKey(name: 'rarity')
+  String rarity;
+
+  @JsonKey(name: 'requiredForUpgrade')
+  dynamic requiredForUpgrade;
+
+  @JsonKey(name: 'icon')
+  String icon;
+
+  @JsonKey(name: 'key')
+  String key;
+
+  @JsonKey(name: 'elixir')
+  int elixir;
+
+  @JsonKey(name: 'type')
+  String type;
+
+  @JsonKey(name: 'arena')
+  int arena;
+
+  @JsonKey(name: 'description')
+  String description;
+
+  @JsonKey(name: 'id')
+  int id;
+
+  CurrentDeck(
+    this.name,
+    this.level,
+    this.maxLevel,
+    this.count,
+    this.rarity,
+    this.requiredForUpgrade,
+    this.icon,
+    this.key,
+    this.elixir,
+    this.type,
+    this.arena,
+    this.description,
+    this.id,
+  );
+
+  factory CurrentDeck.fromJson(Map<String, dynamic> srcJson) =>
+      _$CurrentDeckFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$CurrentDeckToJson(this);
+}
+
+@JsonSerializable()
+class Cards extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'level')
+  int level;
+
+  @JsonKey(name: 'maxLevel')
+  int maxLevel;
+
+  @JsonKey(name: 'count')
+  int count;
+
+  @JsonKey(name: 'rarity')
+  String rarity;
+
+  @JsonKey(name: 'requiredForUpgrade')
+  dynamic requiredForUpgrade;
+
+  @JsonKey(name: 'icon')
+  String icon;
+
+  @JsonKey(name: 'key')
+  String key;
+
+  @JsonKey(name: 'elixir')
+  int elixir;
+
+  @JsonKey(name: 'type')
+  String type;
+
+  @JsonKey(name: 'arena')
+  int arena;
+
+  @JsonKey(name: 'description')
+  String description;
+
+  @JsonKey(name: 'id')
+  int id;
+
+  Cards(
+    this.name,
+    this.level,
+    this.maxLevel,
+    this.count,
+    this.rarity,
+    this.requiredForUpgrade,
+    this.icon,
+    this.key,
+    this.elixir,
+    this.type,
+    this.arena,
+    this.description,
+    this.id,
+  );
+
+  factory Cards.fromJson(Map<String, dynamic> srcJson) =>
+      _$CardsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$CardsToJson(this);
+}
+
+@JsonSerializable()
+class Achievements extends Object {
+  @JsonKey(name: 'name')
+  String name;
+
+  @JsonKey(name: 'stars')
+  int stars;
+
+  @JsonKey(name: 'value')
+  int value;
+
+  @JsonKey(name: 'target')
+  int target;
+
+  @JsonKey(name: 'info')
+  String info;
+
+  Achievements(
+    this.name,
+    this.stars,
+    this.value,
+    this.target,
+    this.info,
+  );
+
+  factory Achievements.fromJson(Map<String, dynamic> srcJson) =>
+      _$AchievementsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$AchievementsToJson(this);
 }
--- lib/model/player.g.dart
+++ lib/model/player.g.dart
@@ -11,12 +11,321 @@ Player _$PlayerFromJson(Map<String, dynamic> json) {
       tag: json['tag'] as String,
       name: json['name'] as String,
       trophies: json['trophies'] as int,
-      rank: json['rank'] as int);
+      rank: json['rank'] as int,
+      arena: json['arena'] == null
+          ? null
+          : Arena.fromJson(json['arena'] as Map<String, dynamic>),
+      clan: json['clan'] == null
+          ? null
+          : Clan.fromJson(json['clan'] as Map<String, dynamic>),
+      stats: json['stats'] == null
+          ? null
+          : Stats.fromJson(json['stats'] as Map<String, dynamic>),
+      games: json['games'] == null
+          ? null
+          : Games.fromJson(json['games'] as Map<String, dynamic>),
+      leagueStatistics: json['leagueStatistics'] == null
+          ? null
+          : LeagueStatistics.fromJson(
+              json['leagueStatistics'] as Map<String, dynamic>),
+      deckLink: json['deckLink'] as String,
+      currentDeck: (json['currentDeck'] as List)
+          ?.map((e) => e == null
+              ? null
+              : CurrentDeck.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      cards: (json['cards'] as List)
+          ?.map((e) =>
+              e == null ? null : Cards.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      achievements: (json['achievements'] as List)
+          ?.map((e) => e == null
+              ? null
+              : Achievements.fromJson(e as Map<String, dynamic>))
+          ?.toList());
 }
 
 Map<String, dynamic> _$PlayerToJson(Player instance) => <String, dynamic>{
       'tag': instance.tag,
       'name': instance.name,
       'trophies': instance.trophies,
-      'rank': instance.rank
+      'rank': instance.rank,
+      'arena': instance.arena,
+      'clan': instance.clan,
+      'stats': instance.stats,
+      'games': instance.games,
+      'leagueStatistics': instance.leagueStatistics,
+      'deckLink': instance.deckLink,
+      'currentDeck': instance.currentDeck,
+      'cards': instance.cards,
+      'achievements': instance.achievements
+    };
+
+Arena _$ArenaFromJson(Map<String, dynamic> json) {
+  return Arena(json['name'] as String, json['arena'] as String,
+      json['arenaID'] as int, json['trophyLimit'] as int);
+}
+
+Map<String, dynamic> _$ArenaToJson(Arena instance) => <String, dynamic>{
+      'name': instance.name,
+      'arena': instance.arena,
+      'arenaID': instance.arenaID,
+      'trophyLimit': instance.trophyLimit
+    };
+
+Clan _$ClanFromJson(Map<String, dynamic> json) {
+  return Clan(
+      json['tag'] as String,
+      json['name'] as String,
+      json['role'] as String,
+      json['donations'] as int,
+      json['donationsReceived'] as int,
+      json['donationsDelta'] as int,
+      json['badge'] == null
+          ? null
+          : Badge.fromJson(json['badge'] as Map<String, dynamic>));
+}
+
+Map<String, dynamic> _$ClanToJson(Clan instance) => <String, dynamic>{
+      'tag': instance.tag,
+      'name': instance.name,
+      'role': instance.role,
+      'donations': instance.donations,
+      'donationsReceived': instance.donationsReceived,
+      'donationsDelta': instance.donationsDelta,
+      'badge': instance.badge
+    };
+
+Badge _$BadgeFromJson(Map<String, dynamic> json) {
+  return Badge(json['name'] as String, json['category'] as String,
+      json['id'] as int, json['image'] as String);
+}
+
+Map<String, dynamic> _$BadgeToJson(Badge instance) => <String, dynamic>{
+      'name': instance.name,
+      'category': instance.category,
+      'id': instance.id,
+      'image': instance.image
+    };
+
+Stats _$StatsFromJson(Map<String, dynamic> json) {
+  return Stats(
+      json['clanCardsCollected'] as int,
+      json['tournamentCardsWon'] as int,
+      json['maxTrophies'] as int,
+      json['threeCrownWins'] as int,
+      json['cardsFound'] as int,
+      json['favoriteCard'] == null
+          ? null
+          : FavoriteCard.fromJson(json['favoriteCard'] as Map<String, dynamic>),
+      json['totalDonations'] as int,
+      json['challengeMaxWins'] as int,
+      json['challengeCardsWon'] as int,
+      json['level'] as int);
+}
+
+Map<String, dynamic> _$StatsToJson(Stats instance) => <String, dynamic>{
+      'clanCardsCollected': instance.clanCardsCollected,
+      'tournamentCardsWon': instance.tournamentCardsWon,
+      'maxTrophies': instance.maxTrophies,
+      'threeCrownWins': instance.threeCrownWins,
+      'cardsFound': instance.cardsFound,
+      'favoriteCard': instance.favoriteCard,
+      'totalDonations': instance.totalDonations,
+      'challengeMaxWins': instance.challengeMaxWins,
+      'challengeCardsWon': instance.challengeCardsWon,
+      'level': instance.level
+    };
+
+FavoriteCard _$FavoriteCardFromJson(Map<String, dynamic> json) {
+  return FavoriteCard(
+      json['name'] as String,
+      json['id'] as int,
+      json['maxLevel'] as int,
+      json['icon'] as String,
+      json['key'] as String,
+      json['elixir'] as int,
+      json['type'] as String,
+      json['rarity'] as String,
+      json['arena'] as int,
+      json['description'] as String);
+}
+
+Map<String, dynamic> _$FavoriteCardToJson(FavoriteCard instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'id': instance.id,
+      'maxLevel': instance.maxLevel,
+      'icon': instance.icon,
+      'key': instance.key,
+      'elixir': instance.elixir,
+      'type': instance.type,
+      'rarity': instance.rarity,
+      'arena': instance.arena,
+      'description': instance.description
+    };
+
+Games _$GamesFromJson(Map<String, dynamic> json) {
+  return Games(
+      json['total'] as int,
+      json['tournamentGames'] as int,
+      json['wins'] as int,
+      json['warDayWins'] as int,
+      (json['winsPercent'] as num)?.toDouble(),
+      json['losses'] as int,
+      (json['lossesPercent'] as num)?.toDouble(),
+      json['draws'] as int,
+      (json['drawsPercent'] as num)?.toDouble());
+}
+
+Map<String, dynamic> _$GamesToJson(Games instance) => <String, dynamic>{
+      'total': instance.total,
+      'tournamentGames': instance.tournamentGames,
+      'wins': instance.wins,
+      'warDayWins': instance.warDayWins,
+      'winsPercent': instance.winsPercent,
+      'losses': instance.losses,
+      'lossesPercent': instance.lossesPercent,
+      'draws': instance.draws,
+      'drawsPercent': instance.drawsPercent
+    };
+
+LeagueStatistics _$LeagueStatisticsFromJson(Map<String, dynamic> json) {
+  return LeagueStatistics(
+      json['currentSeason'] == null
+          ? null
+          : CurrentSeason.fromJson(
+              json['currentSeason'] as Map<String, dynamic>),
+      json['previousSeason'] == null
+          ? null
+          : PreviousSeason.fromJson(
+              json['previousSeason'] as Map<String, dynamic>),
+      json['bestSeason'] == null
+          ? null
+          : BestSeason.fromJson(json['bestSeason'] as Map<String, dynamic>));
+}
+
+Map<String, dynamic> _$LeagueStatisticsToJson(LeagueStatistics instance) =>
+    <String, dynamic>{
+      'currentSeason': instance.currentSeason,
+      'previousSeason': instance.previousSeason,
+      'bestSeason': instance.bestSeason
+    };
+
+CurrentSeason _$CurrentSeasonFromJson(Map<String, dynamic> json) {
+  return CurrentSeason(json['rank'] as int, json['trophies'] as int,
+      json['bestTrophies'] as int);
+}
+
+Map<String, dynamic> _$CurrentSeasonToJson(CurrentSeason instance) =>
+    <String, dynamic>{
+      'rank': instance.rank,
+      'trophies': instance.trophies,
+      'bestTrophies': instance.bestTrophies
+    };
+
+PreviousSeason _$PreviousSeasonFromJson(Map<String, dynamic> json) {
+  return PreviousSeason(json['id'] as String, json['rank'] as int,
+      json['trophies'] as int, json['bestTrophies'] as int);
+}
+
+Map<String, dynamic> _$PreviousSeasonToJson(PreviousSeason instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'rank': instance.rank,
+      'trophies': instance.trophies,
+      'bestTrophies': instance.bestTrophies
+    };
+
+BestSeason _$BestSeasonFromJson(Map<String, dynamic> json) {
+  return BestSeason(
+      json['id'] as String, json['rank'] as int, json['trophies'] as int);
+}
+
+Map<String, dynamic> _$BestSeasonToJson(BestSeason instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'rank': instance.rank,
+      'trophies': instance.trophies
+    };
+
+CurrentDeck _$CurrentDeckFromJson(Map<String, dynamic> json) {
+  return CurrentDeck(
+      json['name'] as String,
+      json['level'] as int,
+      json['maxLevel'] as int,
+      json['count'] as int,
+      json['rarity'] as String,
+      json['requiredForUpgrade'],
+      json['icon'] as String,
+      json['key'] as String,
+      json['elixir'] as int,
+      json['type'] as String,
+      json['arena'] as int,
+      json['description'] as String,
+      json['id'] as int);
+}
+
+Map<String, dynamic> _$CurrentDeckToJson(CurrentDeck instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'level': instance.level,
+      'maxLevel': instance.maxLevel,
+      'count': instance.count,
+      'rarity': instance.rarity,
+      'requiredForUpgrade': instance.requiredForUpgrade,
+      'icon': instance.icon,
+      'key': instance.key,
+      'elixir': instance.elixir,
+      'type': instance.type,
+      'arena': instance.arena,
+      'description': instance.description,
+      'id': instance.id
+    };
+
+Cards _$CardsFromJson(Map<String, dynamic> json) {
+  return Cards(
+      json['name'] as String,
+      json['level'] as int,
+      json['maxLevel'] as int,
+      json['count'] as int,
+      json['rarity'] as String,
+      json['requiredForUpgrade'],
+      json['icon'] as String,
+      json['key'] as String,
+      json['elixir'] as int,
+      json['type'] as String,
+      json['arena'] as int,
+      json['description'] as String,
+      json['id'] as int);
+}
+
+Map<String, dynamic> _$CardsToJson(Cards instance) => <String, dynamic>{
+      'name': instance.name,
+      'level': instance.level,
+      'maxLevel': instance.maxLevel,
+      'count': instance.count,
+      'rarity': instance.rarity,
+      'requiredForUpgrade': instance.requiredForUpgrade,
+      'icon': instance.icon,
+      'key': instance.key,
+      'elixir': instance.elixir,
+      'type': instance.type,
+      'arena': instance.arena,
+      'description': instance.description,
+      'id': instance.id
+    };
+
+Achievements _$AchievementsFromJson(Map<String, dynamic> json) {
+  return Achievements(json['name'] as String, json['stars'] as int,
+      json['value'] as int, json['target'] as int, json['info'] as String);
+}
+
+Map<String, dynamic> _$AchievementsToJson(Achievements instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'stars': instance.stars,
+      'value': instance.value,
+      'target': instance.target,
+      'info': instance.info
     };
